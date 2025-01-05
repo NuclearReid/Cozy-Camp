47950d9d2bc4eb143e0e6afdbe4038e7cf86cda5
Reid Back <reid.backcnmt@gmail.com>
2025-01-05 21:24:52 +1100

    Will now let the user know if their scene is not showing up

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -5,7 +5,6 @@ import { useState } from "react"
 export default function SearchBar() {
     // useLazyQuery is to make it so the query runs when i click the search button instead of when the page loads
     const [findUser, { loading, data, error}] = useLazyQuery(FIND_USER)
-
     const [formState, setFormState] = useState({
         usernameSearch: '',
     })
@@ -13,18 +12,16 @@ export default function SearchBar() {
     const handleFormSubmit = async (event) => {
         event.preventDefault()
 
-        try {
+        // try {
             const searchUser = await findUser({
                 variables: { 
                     username: formState.usernameSearch 
                 }
             })
-            console.log(data)
-            return searchUser           
-
-        } catch (error) {
-            console.error(error)
-        }
+            console.log(searchUser.data?.user)
+            if(!searchUser.data?._id){
+                console.log('there is no user')
+            }
     }
 
     const handleChange = ( event ) => {
@@ -32,7 +29,7 @@ export default function SearchBar() {
         setFormState({
             ...formState,
             [name]: value
-        })
+        })    
     }
 
     return(
@@ -63,8 +60,10 @@ export default function SearchBar() {
             {/* This is just where I can make sure the user info is showing up! */}
             {/* I'll send this data to the scene and have their scene Render */}
             {loading && <p>Loading...</p>}
-            {error && <p>Error: {error.message}</p>}
-            {data && data.user ?(
+            {/* Lets the user know a username was not found */}
+            {(data?.user?._id == null) && <p>Unable to find that username</p>}
+            {/* If a user is found with that username, their info is displayed */}
+            {data?.user?._id && 
                 <div>
                     <h1>User Details</h1>
                     <p>ID: {data.user._id}</p>
@@ -72,9 +71,8 @@ export default function SearchBar() {
                     <p>Username: {data.user.username}</p>
                     <p>Shelter: {data.user.shelter}</p>
                 </div>
-            ): (
-                <h1> Look up a user! </h1>
-            )}
+            }
+
         </>
     )
 }
