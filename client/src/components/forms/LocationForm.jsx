cc67cf0ce5e11cda7e0d66e072ba28aa6a8940a8
Reid Back <reid.backcnmt@gmail.com>
2025-01-29 11:37:51 +1100

    I thought i fixed the bug, I needed to include a onCompleted callback to the mutation to make sure it worked

--- client/src/components/forms/LocationForm.jsx
+++ client/src/components/forms/LocationForm.jsx
@@ -9,11 +9,18 @@ export default function LocationForm({
         currentLocation
     }){
 
-    const [setLocation, {error, data}] = useMutation(SET_LOCATION)
+    const [setLocation, {error, data}] = useMutation(SET_LOCATION, {
+        // I was running into an error where the mutation was not working on the first time submit was clicked when the page loaded. This seems to fix the bug
+        // Basically just makes sure that I'm running the onLocationUpdate after using the mutation
+        onCompleted: (data) => {
+            if(data){
+                onLocationUpdate(formState.location)
+            }
+        }
+    })
 
     // make the limit 58, the longest city has 58 characters it's Llanfair¬pwllgwyngyll¬gogery¬chwyrn¬drobwll¬llan¬tysilio¬gogo¬goch in Wales
     const [charCount, setCharCount] = useState(0) 
-
 
     const [formState, setFormState] = useState({
         location: "",
@@ -66,7 +73,7 @@ export default function LocationForm({
                     className="form-control"
                     name="location"
                     type="text"
-                    placeholder="Where do you camp?"
+                    placeholder="Where are you from?"
                     onChange={handleChange}
                     value={formState.location}
                     maxLength={58}
